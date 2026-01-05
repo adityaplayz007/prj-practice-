76d3e5f1c6666136c7a9e6e21bedad7966875017
adityaplayz007 <adityaprimeera007@gmail.com>
2026-01-05 16:31:44 +0530

    Add files via upload

---
+++ incr.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int rows = 5;      
+    int num = 11;      
+    
+
+    for (int i = 1; i <= rows; i++) {
+        
+       
+        for (int j = 1; j <= i; j++) {
+            printf("%d ", num);
+            num++;     
+        }
+        
+        
+        printf("\n");
+    }
+    
+    return 0;
+}
